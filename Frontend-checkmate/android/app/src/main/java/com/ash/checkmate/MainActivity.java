cc7dcfbc4dc1a98d4fda46583a83fb225eebe70a
Ashutosh <ashutosh1120sharma@gmail.com>
2026-05-18 20:00:37 +0530

    android setup

---
+++ Frontend-checkmate/android/app/src/main/java/com/ash/checkmate/MainActivity.java
@@ -0,0 +1,5 @@
+package com.ash.checkmate;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ Frontend-checkmate/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.ash.checkmate',
+  appName: 'Checkmate',
+  webDir: 'dist/checkmate'
+};
+
+export default config;
